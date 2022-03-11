c2c2cd0714837db42162a69de84168c2c8cd9553
Invincible <87626336+vbmisra@users.noreply.github.com>
2022-03-10 21:28:03 -0800

    modified loginForm

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 // import { loginUser } from '../utils/API';
-import { userMutation } from '@apollo/client'
+import { useMutation } from '@apollo/client'
 import { LOGIN } from '../utils/mutations'
 
 import Auth from '../utils/auth';
@@ -12,6 +12,16 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const [login, { error }] = useMutation(LOGIN)
+
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true)
+    } else {
+      setShowAlert(false)
+    }
+  }, [error])
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -29,19 +39,30 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { loginData } = await login({
+        variables: { ...userFormData },
+      })
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
+      console.log(loginData)
+      Auth.login(loginData.login.token)
+    } catch (error) {
+      console.error(err)
     }
+
+    // try {
+    //   const response = await loginUser(userFormData);
+
+    //   if (!response.ok) {
+    //     throw new Error('something went wrong!');
+    //   }
+
+    //   const { token, user } = await response.json();
+    //   console.log(user);
+    //   Auth.login(token);
+    // } catch (err) {
+    //   console.error(err);
+    //   setShowAlert(true);
+    // }
 
     setUserFormData({
       username: '',
